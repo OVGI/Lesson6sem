770a6f3f7e6a274ef355d433517b96c6262953a3
OVGI <girichevao@gmail.com>
2022-12-18 17:56:31 +0300

    task1 Recurs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Рекурсия
+
+// int a;
+// Console.Write($"Введите число: ");
+// int.TryParse(Console.ReadLine()!, out a);
+
+Recurs(0);
+
+for (int i = 0; i < 10; i++)
+    Console.WriteLine($"{i} Запуск функции");
+
+
+void Recurs(int i)
+{
+    Console.WriteLine($"{i} Запуск функции");
+    i++;
+    if (i < 10)
+    {
+        Recurs(i);
+        Recurs(i + 1);
+    }
+}
+
