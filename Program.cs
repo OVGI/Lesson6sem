a66e393469c5e073a0859196e9afb6ce3b8d567f
OVGI <girichevao@gmail.com>
2022-12-18 20:54:00 +0300

    add task5 переворот дв массива

--- Program.cs
+++ Program.cs
@@ -1,19 +1,29 @@
 ﻿/* Задача 39. Напишите программу , котроая перевернет одномерный массив */
 // Двойной массив
 
-int[] array = new int[5];
+int[,] array = new int[5, 4];
 
-for (int i = 0; i < array.Length; i++)
+for (int i = 0; i < array.GetLength(0); i++)
 {
-    array[i] = new Random().Next(10);
-    Console.Write($"{array[i]} ");
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        array[i, j] = new Random().Next(10);
+        Console.Write($"{array[i, j]} ");
+    }
+    Console.WriteLine();
 }
 
-for (int i = 0; i < array.Length / 2; i++)
-{
-    (array[i], array[array.Length - i - 1]) = (array[array.Length - i - 1], array[i]);
-}
+for (int i = 0; i < array.GetLength(0); i++)
+    for (int j = 0; j < array.GetLength(1) / 2; j++)
+        (array[i, j], array[i, array.GetLength(1) - j - 1]) = (array[i, array.GetLength(1) - j - 1], array[i, j]);
+
 Console.WriteLine();
 
-for (int i = 0; i < array.Length; i++)
-    Console.Write($"{array[i]} ");
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+        Console.Write($"{array[i, j]} ");
+    Console.WriteLine();
+}
+
+
